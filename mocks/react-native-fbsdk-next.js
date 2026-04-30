bc7269f6e7d0b7013d2ef7e395694da1d6d1ce53
natively.dev <api@natively.dev>
2026-04-30 06:24:13 +0000

    fix: Remove Facebook SDK to resolve deploy error

--- mocks/react-native-fbsdk-next.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  Settings: { initializeSDK: () => {}, setAppID: () => {}, setClientToken: () => {} },
-  LoginManager: { logInWithPermissions: async () => ({ isCancelled: true }), logOut: () => {} },
-  AccessToken: { getCurrentAccessToken: async () => null },
-  GraphRequest: function() {},
-  GraphRequestManager: function() { this.addRequest = () => this; this.start = () => {}; },
-};
